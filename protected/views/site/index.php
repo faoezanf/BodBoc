80b4120fc3aec2fc7edb1ae2614383993ca1fd3c
Fauzan Firdaus <faoezanfirdaoes@gmail.com>
2020-02-12 10:13:52 +0700

    Commit 1

---
+++ protected/views/site/index.php
@@ -0,0 +1,516 @@
+<ul class="napbar">
+  	<li><a href="#home">Ingenium</a></li>
+  	<!-- <li><a href="#news">News</a></li>
+  	<li><a href="#contact">Contact</a></li> -->
+  	<li style="float:right"><a class="active" href="#about">Log Out</a></li>
+	<li style="float:right">
+	<div class="input-container">
+	<input class="input-field" id="cari" type="text" placeholder="Find Catalogue...">
+	<i class="fa fa-search icon"></i>
+  	</div></li>
+</ul>
+
+<div class="menu"> 
+	<nobr class="activeMenu"> Monitoring BOD / BOC </nobr>
+	<a href="#" class="admini"> Administration </a>
+</div>
+
+<div class="searchMonitor">
+	<input id="cari2" type="text" placeholder="Search...">
+	<i class="fa fa-search icon2"></i>
+</div>
+
+<div class="orderby">
+	<button id="order1" class="orderbyAktif" onclick="bycompany()"><i class="fa fa-building-o"></i> By Company</button> 
+	<button id="order2" class="orderbyNA" onclick="byperson()"><i class="fa fa-user"></i> By Person</button>
+</div>
+
+<div id="orderbycompany">
+<div class="container">
+	<div class="row">
+		<div class="col-md-4"> 
+			<div class="isiKonten" id="kol1">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+			
+		</div>
+		<div class="col-md-4">
+			<div class="isiKonten" id="kol2">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="isiKonten">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4"> 
+			<div class="isiKonten" id="kol1">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+			
+		</div>
+		<div class="col-md-4">
+			<div class="isiKonten" id="kol2">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="isiKonten">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4"> 
+			<div class="isiKonten" id="kol1">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+			
+		</div>
+		<div class="col-md-4">
+			<div class="isiKonten" id="kol2">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="isiKonten">
+				<div class="text-center">
+					<img class="text-center" src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="70%">
+				</div>
+				<p> <b>PT Telkomsel</b></p>
+				<ul>
+					<li>Tier 1</li>
+					<li>CFU Mobile</li>
+					<li>Tipe AP</li>
+					<li>Wewenang Dwiwarna</li>
+					<li>Saham 51% TLKM 49%</li>
+					<li>Singtel</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<p id="jumData">Total 92 Data</p>
+
+<div class="text-center">
+	<nav aria-label="Page navigation example">
+		<ul class="pagination pagination-lg justify-content-center">
+			<li class="page-item disabled">
+				<a class="page-link" href="" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+					<span class="sr-only">Previous</span>
+				</a>
+			</li>
+			<li class="page-item active" style="background-color:red"><a class="page-link">1</a></li>
+			<li class="page-item"><a class="page-link" href="#">2</a></li>
+			<li class="page-item"><a class="page-link" href="#">3</a></li>
+			<li class="page-item"><a class="page-link" href="#">4</a></li>
+			<li class="page-item"><a class="page-link" href="#">5</a></li>
+			<li class="page-item">
+				<a class="page-link" href="#" aria-label="Next">
+					<span aria-hidden="true">&raquo;</span>
+					<span class="sr-only">Next</span>
+				</a>
+			</li>
+	</ul>
+	</nav>
+</div>
+</div>
+
+<div id="orderbyperson">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br> <br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br> <br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br> <br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br> <br>
+		<div class="row">
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="row" style="cursor:pointer">
+					<div class="col-md-2">
+					<img id="gbr" src="<?php echo Yii::app()->request->baseUrl; ?>/images/person.jpg" width="120%">
+					</div>
+					<div class="col-md-10" style="line-height:35px;">
+						<p style="font-size:17px"><b>Kurniawan Adina K / 930311 </b> <br>
+						Director of HCM PT Telkomsel (I: 2018-2020) <br>
+						Komisaris Utama PT. Telkom Sigma(I: 2018-2020) <br>
+						Komisaris PT. Metranet (I: 2018-2020) </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<p id="jumData">Total 201 Data</p>
+	<div class="text-center">
+	<nav aria-label="Page navigation example">
+		<ul class="pagination pagination-lg justify-content-center">
+			<li class="page-item disabled">
+				<a class="page-link" href="" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+					<span class="sr-only">Previous</span>
+				</a>
+			</li>
+			<li class="page-item active" style="background-color:red"><a class="page-link">1</a></li>
+			<li class="page-item"><a class="page-link" href="#">2</a></li>
+			<li class="page-item"><a class="page-link" href="#">3</a></li>
+			<li class="page-item"><a class="page-link" href="#">4</a></li>
+			<li class="page-item"><a class="page-link" href="#">5</a></li>
+			<li class="page-item">
+				<a class="page-link" href="#" aria-label="Next">
+					<span aria-hidden="true">&raquo;</span>
+					<span class="sr-only">Next</span>
+				</a>
+			</li>
+	</ul>
+	</nav>
+</div>
+</div>
+
+
+<script>
+	function bycompany() {
+		document.getElementById("order2").className = "orderbyNA";
+		document.getElementById("order1").className = "orderbyAktif";
+		document.getElementById("orderbycompany").style.display = "block";
+		document.getElementById("orderbyperson").style.display = "none";
+	}
+
+	function byperson() {
+		document.getElementById("order1").className = "orderbyNA";
+		document.getElementById("order2").className = "orderbyAktif";
+		document.getElementById("orderbycompany").style.display = "none";
+		document.getElementById("orderbyperson").style.display = "block";
+	}
+</script>
+
+
+<!--<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="itemKonten">
+
+			
+			<div> <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="75%"></div>
+			<p> <b>PT Telkomsel</b></p>
+			<ul>
+				<li>Tier 1</li>
+				<li>CFU Mobile</li>
+				<li>Tipe AP</li>
+				<li>Wewenang Dwiwarna</li>
+				<li>Saham 51% TLKM 49%</li>
+				<li>Singtel</li>
+			</ul>
+			</div>
+    </div>
+    <div class="col-md-4">
+	<div class="itemKonten">
+
+			
+<div class=""> <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="75%"></div>
+<p> <b>PT Telkomsel</b></p>
+<ul>
+	<li>Tier 1</li>
+	<li>CFU Mobile</li>
+	<li>Tipe AP</li>
+	<li>Wewenang Dwiwarna</li>
+	<li>Saham 51% TLKM 49%</li>
+	<li>Singtel</li>
+</ul>
+</div>
+    </div>
+    <div class="col-md-4">
+	<div class="itemKonten">
+
+			
+<div class="text-center"> <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="75%"></div>
+<p> <b>PT Telkomsel</b></p>
+<ul>
+	<li>Tier 1</li>
+	<li>CFU Mobile</li>
+	<li>Tipe AP</li>
+	<li>Wewenang Dwiwarna</li>
+	<li>Saham 51% TLKM 49%</li>
+	<li>Singtel</li>
+</ul>
+</div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+	<div class="itemKonten">
+
+			
+<div class="text-center"> <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="75%"></div>
+<p> <b>PT Telkomsel</b></p>
+<ul>
+	<li>Tier 1</li>
+	<li>CFU Mobile</li>
+	<li>Tipe AP</li>
+	<li>Wewenang Dwiwarna</li>
+	<li>Saham 51% TLKM 49%</li>
+	<li>Singtel</li>
+</ul>
+</div>
+    </div>
+    <div class="col-md-4">
+	<div class="itemKonten">
+
+			
+<div class="text-center"> <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="75%"></div>
+<p> <b>PT Telkomsel</b></p>
+<ul>
+	<li>Tier 1</li>
+	<li>CFU Mobile</li>
+	<li>Tipe AP</li>
+	<li>Wewenang Dwiwarna</li>
+	<li>Saham 51% TLKM 49%</li>
+	<li>Singtel</li>
+</ul>
+</div>
+    </div>
+    <div class="col-md-4">
+	<div class="itemKonten">
+
+			
+<div class="text-center"> <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="75%"></div>
+<p> <b>PT Telkomsel</b></p>
+<ul>
+	<li>Tier 1</li>
+	<li>CFU Mobile</li>
+	<li>Tipe AP</li>
+	<li>Wewenang Dwiwarna</li>
+	<li>Saham 51% TLKM 49%</li>
+	<li>Singtel</li>
+</ul>
+</div>
+    </div>
+  </div>
+</div>
+
+	<!-- <div class="hasilKonten"> 
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/telkomsel.png" width="15%">
+	</div?> -->
